1442a4e6f4a5b471e5aec60bd5bf0a4b117bccb5
Deon T. Benton <dtbenton@andrew.cmu.edu>
2018-10-10 17:13:57 -0400

    df

--- pinc - ambig, prev, cause, script.R
+++ pinc - ambig, prev, cause, script.R
@@ -20,7 +20,7 @@ objects_full = combos(20)$binary
 
 
 # PREVENTATIVE CAUSES
-prev_objects_stim = as.data.frame(matrix(NA, nrow=100, ncol=12))
+prev_objects_stim = as.data.frame(matrix(NA, nrow=100, ncol=20))
 for(i in 1:nrow(objects_full)){
   if(objects_full[i,6]==1|objects_full[i,7]==1|objects_full[i,8]==1|objects_full[i,9]==1|objects_full[i,10]==1|objects_full[i,11]==1|objects_full[i,12]==1|objects_full[i,13]==1|objects_full[i,14]==1|objects_full[i,15]==1|objects_full[i,16]==1|objects_full[i,17]==1|objects_full[i,18]==1|objects_full[i,19]==1|objects_full[i,20]==1){
     prev_objects_stim[i,] = NA
@@ -37,7 +37,7 @@ rownames(prev_objects_stim) = c(1:7)
 
 
 # GENERATIVE CAUSES
-gen_objects_stim = as.data.frame(matrix(NA, nrow=nrow(objects_full), ncol=12))
+gen_objects_stim = as.data.frame(matrix(NA, nrow=nrow(objects_full), ncol=20))
 for(i in 1:nrow(objects_full)){
   if(objects_full[i,1]==1|objects_full[i,2]==1|objects_full[i,3]==1|objects_full[i,4]==1|objects_full[i,5]==1|objects_full[i,6]==1|objects_full[i,7]==1|objects_full[i,15]==1){
     gen_objects_stim[i,] = NA
@@ -55,7 +55,7 @@ rownames(gen_objects_stim) = c(1:7)
 
 
 # AMBIGUOUS CAUSES 1
-ambig_objects_stim_1 = as.data.frame(matrix(NA, nrow=nrow(objects_full), ncol=12))
+ambig_objects_stim_1 = as.data.frame(matrix(NA, nrow=nrow(objects_full), ncol=20))
 for(i in 1:nrow(objects_full)){
   if(objects_full[i,1]==1|objects_full[i,2]==1|objects_full[i,3]==1|objects_full[i,4]==1|objects_full[i,5]==1|objects_full[i,6]==1|objects_full[i,7]==1|objects_full[i,8]==1|objects_full[i,9]==1|objects_full[i,10]==1|objects_full[i,11]==1|objects_full[i,12]==1|objects_full[i,13]==1|objects_full[i,14]==1){
     ambig_objects_stim_1[i,] = NA
@@ -72,7 +72,7 @@ names(ambig_objects_stim_1) = NULL
 
 
 # AMBIGUOUS CAUSES 2
-ambig_objects_stim_2 = as.data.frame(matrix(NA, nrow=nrow(objects_full), ncol=12))
+ambig_objects_stim_2 = as.data.frame(matrix(NA, nrow=nrow(objects_full), ncol=20))
 for(i in 1:nrow(objects_full)){
   if(objects_full[i,1]==1|objects_full[i,2]==1|objects_full[i,3]==1|objects_full[i,4]==1|objects_full[i,5]==1|objects_full[i,6]==1|objects_full[i,7]==1|objects_full[i,8]==1|objects_full[i,9]==1|objects_full[i,10]==1|objects_full[i,11]==1|objects_full[i,12]==1|objects_full[i,13]==1|objects_full[i,14]==1){
     ambig_objects_stim_2[i,] = NA
